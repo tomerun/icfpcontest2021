619517bccaf9b336343ad96f16c6483127bb1191
tomerun <tomerunmail@gmail.com>
2021-07-10 17:03:07 +0900

    bugfix

--- tomerun/tools/submitter.cr
+++ tomerun/tools/submitter.cr
@@ -3,7 +3,7 @@ require "http/client"
 headers = HTTP::Headers.new
 headers["Authorization"] = "Bearer #{ENV["API_KEY"]}"
 headers["Content-Type"] = "application/json"
-body = STDIN.readlines.join
+body = STDIN.gets_to_end
 HTTP::Client.post("https://poses.live/api/problems/#{ENV["PROBLEM_ID"]}/solutions", headers: headers, body: body) do |res|
   puts res.status
   puts res.headers
