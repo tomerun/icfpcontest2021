e7eff0122f813c0ed02aa8752780c5c404cf5b73
tomerun <tomerunmail@gmail.com>
2021-07-10 11:23:05 +0900

    前回からの変化を表示、自チームは常に表示

--- tomerun/tools/standings.py
+++ tomerun/tools/standings.py
@@ -1,3 +1,4 @@
+import boto3
 import html
 import json
 import re
@@ -9,21 +10,61 @@ slack_url = "https://hooks.slack.com/services/xxxxxxxxxxx/xxxxxxxxxxx/xxxxxxxxxx
 
 
 def lambda_handler(event, context):
+    s3 = boto3.client("s3")
+    prev = None
+    try:
+        res = s3.get_object(Bucket="icfpc-2021", Key="standings/latest.json")
+        prev_teams = json.loads(res["Body"].read().decode())
+        prev = {t[1]: (t[0], t[2]) for t in prev_teams}
+    except Exception as e:
+        print(e)
+        pass
     req = request.Request(standings_url)
     with request.urlopen(req) as res:
         now = datetime.now() + timedelta(hours=9)
         now = now.replace(microsecond=0)
         body = html.unescape(res.read().decode())
-        teams = []
+        teams_all = []
         for m in re.finditer(r"<tr><td>(\d+)</td><td>(.*?)</td><td>(\d+)</td></tr>", body):
-            teams.append((m[1], m[2], m[3]))
-    print(teams)
-    teams = teams[:30]
-    len_place = max(len(t[0]) for t in teams)
+            teams_all.append([m[1], m[2], m[3]])
+    # print(teams_all)
+    s3.put_object(
+        Bucket="icfpc-2021",
+        Key=f"standings/{now.strftime('%Y%m%d_%H%M%S')}.json",
+        Body=json.dumps(teams_all).encode())
+    s3.put_object(
+        Bucket="icfpc-2021", Key="standings/latest.json", Body=json.dumps(teams_all).encode())
+
+    teams = []
+    for i, t in enumerate(teams_all):
+        if i < 25 or t[1] == "Independent Set":
+            teams.append(t)
+    if prev:
+        for t in teams:
+            if t[1] not in prev:
+                continue
+            prev_rank, prev_score = prev[t[1]]
+            diff = int(t[0]) - int(prev_rank)
+            if diff > 0:
+                diff = f"▼{diff}"
+            elif diff < 0:
+                diff = f"△{abs(diff)}"
+            else:
+                diff = "0"
+            t[0] = f"{t[0]}({diff})"
+
+            diff = int(t[2]) - int(prev_score)
+            if diff > 0:
+                diff = f"+{diff}"
+            else:
+                diff = str(diff)
+            t.append(f"({diff})")
+
+    len_place = max(len(t[0]) for t in teams) + 1
     len_name = max(len(t[1]) for t in teams)
     len_score = max(len(t[2]) for t in teams)
-    fmt = f"%{len_place}s %{len_name}s %{len_score}s"
-    standings = "\n".join(fmt % t for t in teams)
+    fmt = f"%{len_place}s  %{len_name}s  %{len_score}s%s"
+    standings = "\n".join(fmt % tuple(t) for t in teams)
     # print(standings)
     standings = html.escape(standings)
     message = json.dumps({
