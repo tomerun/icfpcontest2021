13d68d4b9919836b6b49a49994ef078aeaab9283
tomerun <tomerunmail@gmail.com>
2021-07-11 04:16:02 +0900

    改善した物だけをサブミットしてくれる君

--- tomerun/tools/submitter.cr
+++ tomerun/tools/submitter.cr
@@ -1,11 +1,32 @@
 require "http/client"
+PROB_COUNT = 88
+
+def read_scores(file_name)
+  scores = Array.new(PROB_COUNT + 1, 1e100)
+  File.read_lines(file_name).each do |line|
+    if line.strip =~ /(\d+)\s*dislike.(.+)/
+      seed = $1.to_i
+      score = $2.to_f
+      scores[seed] = score
+    end
+  end
+  return scores
+end
+
+my_score = read_scores("../result/best/scores.txt")
+team_score = read_scores(ARGV[0])
 
 headers = HTTP::Headers.new
 headers["Authorization"] = "Bearer #{ENV["API_KEY"]}"
 headers["Content-Type"] = "application/json"
-body = STDIN.gets_to_end
-HTTP::Client.post("https://poses.live/api/problems/#{ENV["PROBLEM_ID"]}/solutions", headers: headers, body: body) do |res|
-  puts res.status
-  puts res.headers
-  puts res.body
+
+1.upto(PROB_COUNT) do |i|
+  next if my_score[i] >= team_score[i]
+  puts "submit #{i} #{team_score[i]} => #{my_score[i]}"
+  body = File.open("../result/best/#{sprintf("%04d", i)}.json").gets_to_end
+  HTTP::Client.post("https://poses.live/api/problems/#{i}/solutions", headers: headers, body: body) do |res|
+    puts res.status
+    puts res.headers
+    puts res.body
+  end
 end
