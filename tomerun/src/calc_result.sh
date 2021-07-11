838171f0226ea6a529833fe59cfbeb07e858c1e5
tomerun <tomerunmail@gmail.com>
2021-07-11 20:12:11 +0900

    logging

--- tomerun/src/calc_result.sh
+++ tomerun/src/calc_result.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -eux
+#!/bin/bash -eu
 
 AWS_ID=$1
 RESULT_DIR="../result/$AWS_ID"
@@ -7,6 +7,7 @@ aws s3 cp --recursive s3://marathon-tester/ICFPC2021/$AWS_ID/00 $RESULT_DIR
 crystal build validator.cr
 for (( i = 1; i <= 106; i++ )); do
 	id=$(printf "%04d" $i)
+	echo $id
 	echo -n "$id " >> $RESULT_DIR/scores.txt
 	./validator ../../problems/$id.json $RESULT_DIR/$id.json >> $RESULT_DIR/scores.txt 
 done
